7c857f1c9268fe7972879b6703f3e26941e89ffe
student <omer.hemo8000@gmail.com>
2020-01-09 19:48:31 +0000

    fgh

--- main1.asm
+++ main1.asm
@@ -1,10 +1,22 @@
 .extern a, b, c
 .global main
+.data
+a: .long 8
+b: .long 8
+c: .space 8
 
 .text
-.abort
+_start:
+	call main
+
+	# exit(0)
+	mov $60, %rax
+	xor %rdi, %rdi
+	syscall
+
 main:
-    movl a, %eax
-    imull b
-    movq %rax, c   
-    ret
+	.abort
+	movl a, %eax
+	imull b
+	movq %rax, c
+	ret
